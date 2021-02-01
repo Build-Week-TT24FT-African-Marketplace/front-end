6f77e8886168e5a4a17eb926110b8ff509351661
Wei Wu <weidiwu207@gmail.com>
2021-02-01 16:33:42 -0500

    setting up forms (#1)
    * setting up forms
    
    * setting up more

---
+++ african-marketplace/src/components/NewItemForm.js
@@ -0,0 +1,48 @@
+import React from 'react'
+
+export default function ItemForm(props){
+
+const {values, onChange, onSubmit, disabled} = props
+
+const onChange = (evt) => {
+    const { name, value} = evt.target;
+    change(name, value);
+  };
+const onSubmit = (evt) => {
+    evt.preventDefault()
+    onSubmit();
+}
+return (
+<div>
+    <form onSubmit={onSubmit} >
+
+        <label>
+            Item Name
+        <input name="itemName" value={values.name} type="text" onChange={onChange}> </input>
+        </label>
+
+        <label>
+            Description
+        <input name="description" value={values.description} type="text" onChange={onChange}>  </input>
+        </label>
+
+        <label>
+            Price
+        <input name="Price" value={values.price} type="number" step="0.01" onChange={onChange}> </input>
+        </label>
+
+        <label>
+            Location
+        <select onChange={} value={values.location} name="location">
+            <option value="">- Select an location -</option>
+            <option value="1"></option>
+            <option value="2"></option>
+            <option value="3"></option>
+            <option value="4"></option>
+          </select>
+        </label>
+        <button disabled={disabled}> Submit</button>
+    </form>
+
+</div>)
+}
