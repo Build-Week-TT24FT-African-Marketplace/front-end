34d45b3e512a90aa51b9090d9a8aee08537871f1
Your Name <you@example.com>
2021-02-02 13:26:22 -0600

    Missing Pages Added

---
+++ african-marketplace/src/components/UserItemList.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const UserItemList = () => {
+  //add item button -> displays add item component, HERE ON THIS PAGE!
+
+  //edit button -> displays edit item component, ON ITEM CARD - with turnary!
+  //delete button -> deletes item component, ON ITEM CARD!
+  
+  return (
+    <div>
+      LIST WITH BTNs
+      <div></div>
+    </div>
+  );
+};
+
+export default UserItemList;
