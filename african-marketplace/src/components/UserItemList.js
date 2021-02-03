dee20007531c6f6193d51f3655a5dad8418cdf04
Wei Wu <weidiwu207@gmail.com>
2021-02-03 01:20:59 -0500

    setted up Itemlist and ItemCard components (#5)

--- african-marketplace/src/components/UserItemList.js
+++ african-marketplace/src/components/UserItemList.js
@@ -1,15 +1,22 @@
 import React from "react";
+import ItemCard from './ItemCard'
+const UserItemList = (props) => {
 
-const UserItemList = () => {
+  const {item_list, role} = props;
   //add item button -> displays add item component, HERE ON THIS PAGE!
-
   //edit button -> displays edit item component, ON ITEM CARD - with turnary!
   //delete button -> deletes item component, ON ITEM CARD!
   
+
   return (
     <div>
-      LIST WITH BTNs
-      <div></div>
+      <div>
+        {item_list.map((item) => {
+          <ItemCard item={item} role={buyer}/>
+        })}
+      </div>
+      {/* {Add item button is shown based on role 1 = seller 2 = buyer role is set to 1 by default} */}
+     { (role == 2 ) ? `${<button> Add Item </button>}` : null}
     </div>
   );
 };
